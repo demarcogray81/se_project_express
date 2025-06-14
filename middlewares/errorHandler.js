dae1901db2f8fe32b325466bdc5582bdab2fed2f
PrinceAhbar <demarcogray81@gmail.com>
2025-06-13 22:26:29 -0400

    Project 12 Auth, Controller, Routes, Middleware, and Error handling UPDATE

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,6 @@ const { SERVER_ERROR } = require("../utils/errors");
 module.exports = (err, req, res) => {
   const { statusCode = SERVER_ERROR, message } = err;
 
-  console.error(`Error ${err.name || "UnknownError"} - ${err.message}`);
   res.status(statusCode).send({
     message:
       statusCode === SERVER_ERROR
