dc32dd8c58dad987079297a148de4c5d11e9fb33
PrinceAhbar <demarcogray81@gmail.com>
2025-06-13 22:09:54 -0400

    Project 12 Auth, Controller, Routes, Middleware, and Error handling UPDATE

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const { PORT = 3001 } = process.env;
