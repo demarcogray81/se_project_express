3bddbf90802a00469e64cd9fb8e2a199f98e1941
PrinceAhbar <demarcogray81@gmail.com>
2025-06-16 20:29:45 -0400

    Added Profile page and styled

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,7 @@ const { SERVER_ERROR } = require("../utils/errors");
 module.exports = (err, req, res) => {
   const { statusCode = SERVER_ERROR, message } = err;
 
-  res.status(statusCode).send({
+  res.status(statusCode).json({
     message:
       statusCode === SERVER_ERROR
         ? "An error has occurred on the server"
