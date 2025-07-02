2c10199b5732ba7a88586356c792a132ae933aa9
PrinceAhbar <demarcogray81@gmail.com>
2025-07-02 19:40:15 -0400

    Integrated authorization 0.1.1

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -9,7 +8,17 @@ const { PORT = 3001 } = process.env;
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === "test") {
+  app.use((req, res, next) => {
+    req.user = {
+      _id: "5d8b8592978f8bd833ca8133",
+    };
+    next();
+  });
+}
+
 app.use(routes);
+
 app.use(errorHandler);
 
 mongoose
